81e363dd3e95bffcdd1d3ebf27acddbefe668cf8
AkaashSamson <akaashsam22@gmail.com>
2024-09-08 23:30:47 +0530

    Able to display movie list

--- main.py
+++ main.py
@@ -21,11 +21,49 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+class base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=base)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 Bootstrap5(app)
 
-# CREATE DB
+db.init_app(app)
+
+class Movie(db.Model):
+    id : Mapped[int] = mapped_column(Integer, primary_key=True)
+    title : Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    year : Mapped[int] = mapped_column(Integer, nullable=False)
+    description : Mapped[str] = mapped_column(String, nullable=False)
+    rating : Mapped[float] = mapped_column(Float, nullable=False)
+    ranking : Mapped[int] = mapped_column(Integer, nullable=False)
+    review : Mapped[str] = mapped_column(String, nullable=False)
+    img_url : Mapped[str] = mapped_column(String, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+#ADDING A MOVIE
+# new_movie = Movie(
+#     title="Up",
+#     year=2009,
+#     description="Seventy-eight-year-old Carl Fredricksen travels to Paradise Falls in his house equipped with balloons, inadvertently taking a young stowaway.",
+#     rating=8.2,
+#     ranking=9,
+#     review="One of the most emotional movies to ever exist.",
+#     img_url="https://upload.wikimedia.org/wikipedia/en/0/05/Up_%282009_film%29.jpg"
+# )
+  
+# with app.app_context():
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+
+
+# CREA DB
 
 
 # CREATE TABLE
@@ -33,7 +71,10 @@ Bootstrap5(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+        movies = db.session.execute(db.select(Movie))
+        all_movies = movies.scalars()
+        return render_template("index.html", movies=all_movies)
 
 
 if __name__ == '__main__':
